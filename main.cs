80e26477985e469cb7eaf71c9c2ab6e4a16e0f58
Felipe Lazzarini <103012070+Muowl@users.noreply.github.com>
2022-04-13 10:12:23 +0000

    Added Exception treatment

--- main.cs
+++ main.cs
@@ -5,13 +5,19 @@ class Program
 {
   public static void Main (string[] args) 
   {
-    Console.WriteLine ("Enter the number of points betted for player one: ");
-    float playerOne = float.Parse(Console.ReadLine());
-    Console.WriteLine ("Enter the number of points betted for player two: ");
-    float playerTwo = float.Parse(Console.ReadLine());
-
-    Calculator calc = new Calculator (playerOne, playerTwo);
-    calc.Calculation();
-    
+    try
+    {
+      Console.WriteLine ("Enter the number of points betted for player one: ");
+      float playerOne = float.Parse(Console.ReadLine());
+      Console.WriteLine ("Enter the number of points betted for player two: ");
+      float playerTwo = float.Parse(Console.ReadLine());
+      Calculator calc = new Calculator (playerOne, playerTwo);
+      calc.Calculation();
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine("Error: " + ex.Message);
+    }
+        
   }
 }
